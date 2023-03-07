bbf93158b51a9318e52a9a26b3dc2d87ef7acdb0
asiahhh <125533782+asiahhh@users.noreply.github.com>
2023-03-07 21:03:11 +0800

    Create task6.py

---
+++ specimenp1/task6.py
@@ -0,0 +1,16 @@
+a = input()
+b = input()
+c = input()
+d = input()
+e = input()
+
+if a == 'Ellie':
+    print('1st')
+elif b == 'Ellie':
+    print('2nd')
+elif c == 'Ellie':
+    print('3rd')
+elif d == 'Ellie':
+    print('4th')
+elif e == 'Ellie':
+    print('5th')
